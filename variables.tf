373e20375a488e3c40eb85061640e4fdf22221c5
JohannesBrogen <johannesbrogen@outlook.com>
2024-06-12 08:59:50 +0200

    refactor: Added new vnets object to allow multiple vnets and associated subnets at once

--- variables.tf
+++ variables.tf
@@ -10,6 +10,34 @@ variable "prefix" {
   default     = "Terraform-practice"
 }
 
+variable "vnets" {
+  description = "Vnet address space and connected subnets"
+  type = map(object({
+    address_space = string
+    subnets = list(object({
+      subnet_name    = string
+      subnet_address = string
+    }))
+  }))
+
+  default = {
+    "terraform-practice-vnet" = {
+      address_space = "10.0.0.0/16"
+      subnets = [
+        {
+          subnet_name    = "terraform-practice-subnet1"
+          subnet_address = "10.0.1.0/24"
+        },
+        {
+          subnet_name    = "terraform-practice-subnet2"
+          subnet_address = "10.0.2.0/24"
+        },
+      ]
+    }
+    # Insert new vnet here (remember comma^)
+  }
+}
+
 variable "resource_tags" {
   description = "Tags to set for resources"
   type        = map(string)
