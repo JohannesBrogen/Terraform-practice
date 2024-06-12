2d44d5ee3b19c0e89b4d41173b4dd99b8ada44bd
JohannesBrogen <johannesbrogen@outlook.com>
2024-06-12 10:29:51 +0200

    feat: Added ip outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,12 @@
 output "subnet_ids" {
   description = "Outputs all the subnet ids"
-  value = {for k, v in azurerm_subnet.terraform-subnets : k => v.id}
+  value       = { for k, v in azurerm_subnet.terraform-subnets : k => v.id }
+}
+
+output "linux_private_ip" {
+  value = azurerm_linux_virtual_machine.terraform-linux-vm.private_ip_address
+}
+
+output "linux_public_ip" {
+  value = azurerm_linux_virtual_machine.terraform-linux-vm.public_ip_address
 }
