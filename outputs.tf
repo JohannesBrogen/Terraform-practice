0f93f79d112ebb308ceab00c92cc6b414b8be05e
JohannesBrogen <johannesbrogen@outlook.com>
2024-06-13 13:32:44 +0200

    removed subnet_ids output from root module

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,3 @@
-/*
-output "subnet_ids" {
-  description = "Outputs all the subnet ids"
-  value       = module.network.subnet_ids
-}
-*/
-
 output "linux_private_ip" {
   value = azurerm_linux_virtual_machine.terraform-linux-vm.private_ip_address
 }
